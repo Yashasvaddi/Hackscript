62b9a484fc5d02be6d21ac7c02f6fc4ddec1dc66
Yashasvaddi <yashas.vaddi12@gmail.com>
2025-03-08 12:59:29 +0530

    Basic flask file

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Hello world")
+from flask import Flask,render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__=='__main_':
+    app.run(debug=True)
